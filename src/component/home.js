aad3891a80584bddc5075bb53b204b8d333ed460
Aaron Whang <tlbhibiki@gmail.com>
2020-02-03 18:07:43 -0800

    added about me page template

--- src/component/home.js
+++ src/component/home.js
@@ -1,33 +1,27 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid';
+import '../App.css';
 
 function Home() {
+
+  let checkers = []
+  let color = "even"
+  let stuff = ""
+  for (let i = 1; i <= 120; i++) {
+      if(i % 2 === 1) color = "odd"
+      if(i % 2 === 0) color = "even"
+
+    let stuff = "element element-"+ i + " " + color
+    checkers.push(<div class={stuff}>{i}</div>)
+  }
   return (
-    <div className="Home">
+    <div id="table">
 
-        <p>
-          Aaron Whang's Portfolio
-        </p>
-        <Grid
-          container
-          direction="row"
-          justify="left"
-          alignItems="left"
-        >
-          <Grid container item xs={12} spacing={0} justify="center">
-              <h1> Welcome! </h1>
-
-          </Grid>
-          <Grid container item xs={6} spacing={0} justify="center">
-
-              <p>What is that over there? I see a piece of string cheese. I do wonder if you can make mozzerella sticks by battering and frying string cheese. It seems easy and tasty. </p>
-
-          </Grid>
-          <Grid container item xs={6} spacing={0} justify="center">
-
-              <p>This is a placeholder. What is a placeholder? Well it's an element in order to fill out a space and is designed to test the layout with elements that are meant to be replaced. So that means I am dispensable. Life is fleeting.</p>
-          </Grid>
-        </Grid>
+          <div class="bar">  </div>
+          <div class="title"> Aaron Whang </div>
+          <div class="logo"> </div>
+          <div class="info"> </div>
+          <div class="skills"> </div>
 
 
     </div>
